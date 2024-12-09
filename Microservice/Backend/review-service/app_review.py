ca863969dbe7bbd720e89f42dc1593de08ca41ab
undercoverprincess13 <ellen.kistner@online.de>
2024-12-09 19:27:52 +0100

    chore: prepare "Developer-0" branch for developer group

--- Microservice/Backend/review-service/app_review.py
+++ Microservice/Backend/review-service/app_review.py
@@ -29,58 +29,6 @@ app.add_middleware(
 Reviews_File = "data/rating_and_review.json"
 
 
-# Helper functions::
-def calculate_rating(product_id):
-    """
-    Calculate the rating of a product based on the reviews
-    :param product_id:
-    :return: rating for product with product_id
-    """
-    products = load_data(Reviews_File)
-    total_rating = 0
-    for product in products:
-        if product["id"] == product_id:
-            for review in product["reviews"]:
-                total_rating += review["rating"]
-            review_count = len(product["reviews"])
-            with open(Reviews_File, "w") as file:
-                product["rating"] = total_rating / len(product["reviews"]) if review_count > 0 else 0
-                json.dump(products, file, indent=4)
-            return total_rating / len(product["reviews"]) if review_count > 0 else 0
-
-    return -1
-
-
-def load_data(name):
-    """
-    Load data from a json file
-    :param name:
-    :return: file content
-    """
-    with open(name) as file:
-        return json.load(file)
-
-
-def save_review(review, product_id):
-    """
-    Save a review to a product (product_id)
-    :param review:
-    :param product_id:
-
-    """
-    if os.path.exists(Reviews_File):
-        with open(Reviews_File, "r") as file:
-            products = json.load(file)
-            for product in products:
-                if product["id"] == product_id:
-                    product["reviews"].append(
-                        {"reviewerName": review.reviewerName, "reviewerEmail": review.reviewerEmail,
-                         "rating": review.rating, "comment": review.comment,
-                         "date": str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ"))})
-                    product["rating"] = calculate_rating(product_id)
-                    break
-        with open(Reviews_File, "w") as file:
-            json.dump(products, file, indent=4)
 
 
 @app.get("/{product_id}/rating")
@@ -90,11 +38,6 @@ def get_rating(product_id: int):
     :param product_id:
     :return: rating of the product
     """
-    # Lade Daten aus der JSON-Datei
-    ratings = load_data(Reviews_File)
-    for rating in ratings:
-        if rating["id"] == product_id:
-            return rating["rating"]
 
 
 @app.get("/{product_id}/reviews")
@@ -104,15 +47,7 @@ def get_reviews(product_id: int):
     :param product_id:
     :return: the 3 newest reviews of the product
     """
-    # Lade Daten aus der JSON-Datei
-    reviews = load_data(Reviews_File)
-    print(reviews)
-    for review in reviews:
-        if review["id"] == product_id:
-            # Sort the reviews by date
-            review["reviews"].sort(key=lambda x: datetime.strptime(x['date'], "%Y-%m-%dT%H:%M:%S.%fZ"), reverse=True)
-            # Return the first 3 reviews
-            return review["reviews"][:4]
+
 
 
 @app.post("/{product_id}/review")
@@ -123,13 +58,7 @@ async def add_review(product_id: int, review: ReviewRequest):
     :param review:
     :return: message if the review was saved successfully
     """
-    try:
-        if not review:
-            raise HTTPException(status_code=400, detail="Missing 'email' field")
-        save_review(review, product_id)
-        return {"message": "Review saved successfully"}
-    except json.JSONDecodeError:
-        raise HTTPException(status_code=400, detail="Invalid JSON format")
+
 
 
 if __name__ == "__main__":
